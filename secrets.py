6e1374bac08dcf4d541b0aa5e4bde64ccf4f352b
tijareharshal <60662235+tijareharshal@users.noreply.github.com>
2021-10-02 00:00:38 +0530

    Update secrets.py

--- secrets.py
+++ secrets.py
@@ -1,7 +1,7 @@
 def get_clientID():
-    client_id = '93b88feb0c09474ca2971dbc4532cfd2'
+    client_id = '*******'
     return client_id
 
 def get_clientSecret():
-    client_secret = '6e11790515004787aac215ec125862f3'
+    client_secret = '*******'
     return client_secret
